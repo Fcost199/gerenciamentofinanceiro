3cf2e3c4910358def463d8e41970f05a83bea38c
unknown <costasfernando199@gmail.com>
2020-02-28 14:22:35 -0300

    Inicio!

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { GeralComponent } from './componentes/geral/geral.component';
+import { EntradasComponent } from './componentes/entradas/entradas.component';
+import { SaidasComponent } from './componentes/saidas/saidas.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: GeralComponent},
+  {path: 'entradas', component: EntradasComponent},
+  {path: 'saidas', component: SaidasComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GeralComponent } from './componentes/geral/geral.component';
+import { EntradasComponent } from './componentes/entradas/entradas.component';
+import { SaidasComponent } from './componentes/saidas/saidas.component';
+import { PrincipalHeaderComponent } from './componentes/principal-header/principal-header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GeralComponent,
+    EntradasComponent,
+    SaidasComponent,
+    PrincipalHeaderComponent
   ],
   imports: [
     BrowserModule,
